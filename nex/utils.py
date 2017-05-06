deca1639c171ebe7491fa8aff433ec9b68f38985
Elliot Marsden <elliot.marsden@gmail.com>
2017-05-06 20:55:16 +0200

    Remove unused utility function

--- nex/utils.py
+++ nex/utils.py
@@ -6,11 +6,6 @@ import uuid
 ascii_characters = ''.join(chr(i) for i in range(128))
 
 
-def increasing_window(a):
-    for i_max in range(len(a) + 1):
-        yield a[:i_max]
-
-
 def get_unique_id():
     raw = uuid.uuid4()
     compressed = base64.urlsafe_b64encode(raw.bytes).decode("utf-8")
