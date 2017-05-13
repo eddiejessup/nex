426ac5c2110ab3564bb96c3e6150fba560af9cda
Elliot Marsden <elliot.marsden@gmail.com>
2017-05-13 20:03:54 +0200

    Separate defining quantity variables

--- nex/parsing/common_parsing.py
+++ nex/parsing/common_parsing.py
@@ -70,53 +70,54 @@ common_terminal_types = instructions_to_types(common_terminal_instructions)
 pg = ParserGenerator(common_terminal_types, cache_id="changeme")
 
 
-@pg.production('token_variable : token_register')
-@pg.production('mu_glue_variable : mu_skip_register')
-@pg.production('glue_variable : skip_register')
-@pg.production('dimen_variable : dimen_register')
-@pg.production('integer_variable : count_register')
-def quantity_variable_register(p):
-    return p[0]
+def add_variable_parsing(pg):
+    @pg.production('token_variable : token_register')
+    @pg.production('mu_glue_variable : mu_skip_register')
+    @pg.production('glue_variable : skip_register')
+    @pg.production('dimen_variable : dimen_register')
+    @pg.production('integer_variable : count_register')
+    def quantity_variable_register(p):
+        return p[0]
+
+    @pg.production('token_variable : TOKEN_PARAMETER')
+    @pg.production('mu_glue_variable : MU_GLUE_PARAMETER')
+    @pg.production('glue_variable : GLUE_PARAMETER')
+    @pg.production('dimen_variable : DIMEN_PARAMETER')
+    @pg.production('integer_variable : INTEGER_PARAMETER')
+    def quantity_variable_parameter(p):
+        return p[0]
+
+    @pg.production('token_register : TOKS number')
+    @pg.production('mu_skip_register : MU_SKIP number')
+    @pg.production('skip_register : SKIP number')
+    @pg.production('dimen_register : DIMEN number')
+    @pg.production('count_register : COUNT number')
+    def quantity_register_explicit(p):
+        return BuiltToken(type_=p[0].type, value=p[1], position_like=p)
+
+    @pg.production('token_register : TOKS_DEF_TOKEN')
+    @pg.production('mu_skip_register : MU_SKIP_DEF_TOKEN')
+    @pg.production('skip_register : SKIP_DEF_TOKEN')
+    @pg.production('dimen_register : DIMEN_DEF_TOKEN')
+    @pg.production('count_register : COUNT_DEF_TOKEN')
+    def register_token(p):
+        reg_type = short_hand_reg_def_token_type_to_reg_type[p[0].type]
+        number = p[0].value
+
+        # A bit of token wrangling to make the result look the same as, for
+        # example, `SKIP number`.
+
+        sign = '+' if number >= 0 else '-'
+        sign_tok = BuiltToken(type_='sign', value=sign)
+
+        size_value_tok = BuiltToken(type_='internal', value=abs(number))
+        size_tok = BuiltToken(type_='size', value=size_value_tok)
+        nr_tok = BuiltToken(type_='number', value={'sign': sign_tok,
+                                                   'size': size_tok})
+        return BuiltToken(type_=reg_type, value=nr_tok, position_like=p)
 
 
-@pg.production('token_variable : TOKEN_PARAMETER')
-@pg.production('mu_glue_variable : MU_GLUE_PARAMETER')
-@pg.production('glue_variable : GLUE_PARAMETER')
-@pg.production('dimen_variable : DIMEN_PARAMETER')
-@pg.production('integer_variable : INTEGER_PARAMETER')
-def quantity_variable_parameter(p):
-    return p[0]
-
-
-@pg.production('token_register : TOKS number')
-@pg.production('mu_skip_register : MU_SKIP number')
-@pg.production('skip_register : SKIP number')
-@pg.production('dimen_register : DIMEN number')
-@pg.production('count_register : COUNT number')
-def quantity_register_explicit(p):
-    return BuiltToken(type_=p[0].type, value=p[1], position_like=p)
-
-
-@pg.production('token_register : TOKS_DEF_TOKEN')
-@pg.production('mu_skip_register : MU_SKIP_DEF_TOKEN')
-@pg.production('skip_register : SKIP_DEF_TOKEN')
-@pg.production('dimen_register : DIMEN_DEF_TOKEN')
-@pg.production('count_register : COUNT_DEF_TOKEN')
-def register_token(p):
-    reg_type = short_hand_reg_def_token_type_to_reg_type[p[0].type]
-    number = p[0].value
-
-    # A bit of token wrangling to make the result look the same as, for
-    # example, `SKIP number`.
-
-    sign = '+' if number >= 0 else '-'
-    sign_tok = BuiltToken(type_='sign', value=sign)
-
-    size_value_tok = BuiltToken(type_='internal', value=abs(number))
-    size_tok = BuiltToken(type_='size', value=size_value_tok)
-    nr_tok = BuiltToken(type_='number', value={'sign': sign_tok,
-                                               'size': size_tok})
-    return BuiltToken(type_=reg_type, value=nr_tok, position_like=p)
+add_variable_parsing(pg)
 
 
 @pg.production('mu_glue : internal_mu_glue')
@@ -175,13 +176,9 @@ def coerced_integer_glue(p):
     raise NotImplementedError
 
 
-@pg.production('normal_integer : internal_integer')
-def normal_integer_internal_integer(p):
-    return p[0]
-
-
 @pg.production('normal_dimen : internal_dimen')
-def normal_dimen_internal_dimen(p):
+@pg.production('normal_integer : internal_integer')
+def normal_scalar_internal_scalar(p):
     return p[0]
 
 
