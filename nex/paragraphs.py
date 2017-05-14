486d2cb7d756c783a52ea21c878c6ad01b374db4
Elliot Marsden <elliot.marsden@gmail.com>
2017-05-14 20:44:15 +0200

    Clean up and clarify paragraph logic

--- nex/paragraphs.py
+++ nex/paragraphs.py
@@ -1,4 +1,10 @@
+from collections import namedtuple
+
 from .box import HBox, UnSetGlue
+from .feedback import truncate_list
+
+
+HListKey = namedtuple('HListKey', ('contents', 'badness'))
 
 
 def grab_word(h_list):
@@ -12,32 +18,35 @@ def grab_word(h_list):
     return chars, break_glue
 
 
-def h_list_to_h_box_tree(h_list, w):
+def h_list_to_box_tree(remaining, w):
     tree = {}
-    h_box = HBox(contents=[], to=w, set_glue=False)
-    conts = h_box.contents
-    while h_list:
-        chars, break_glue = grab_word(h_list)
-        conts.extend(chars)
-        if h_box.badness() < 1000:
-            key = (tuple(h_box.contents[:]), len(conts), h_box.badness())
-            tree[key] = h_list_to_h_box_tree(h_list.copy(), w)
+    seen_box = HBox(contents=[], to=w, set_glue=False)
+    seen_conts = seen_box.contents
+    while remaining:
+        grab_chars, grab_break_glue = grab_word(remaining)
+        seen_conts.extend(grab_chars)
+        seen_badness = seen_box.badness()
+        if seen_badness < 1000:
+            key = HListKey(contents=tuple(seen_conts[:]),
+                           badness=seen_badness)
+            tree[key] = h_list_to_box_tree(remaining.copy(), w)
         # If we are not breaking, put the break glue on the list.
-        conts.append(break_glue)
-    key = (tuple(h_box.contents[:]), len(conts), h_box.badness())
+        seen_conts.append(grab_break_glue)
+    # Add possibility to never break this h-list.
+    key = HListKey(contents=tuple(seen_conts[:]), badness=seen_box.badness())
     tree[key] = None
     return tree
 
 
 def pp(tree, l=1):
-    # print(l)
+    tabs = '\t' * l
+    if tree is None:
+        print(f'B: ----- ', tabs, 'end')
+        return
     for k, v in tree.items():
-        tabs = ''.join('\t' for _ in range(l))
-        print(tabs, ' ', k[0][-10:])
-        if v is None:
-            print(tabs, '\t', 'end')
-        else:
-            pp(v, l=l+1)
+        cnt = truncate_list(k.contents, 5)
+        print(f'B: {k.badness} ', tabs, cnt)
+        pp(v, l=l+1)
 
 
 def get_best_route(node, tree, w):
@@ -48,11 +57,11 @@ def get_best_route(node, tree, w):
         else:
             child_best_route = get_best_route(child_node, child_tree, w)
         route_options.append(child_best_route)
-    best_route = min(route_options, key=lambda x: x[0][2])
+    best_route = min(route_options, key=lambda x: x[0].badness)
     # Have the best option, now need to add the sub-route's badness
     # to this node,
-    node = list(node)
-    node[2] += best_route[0][2]
+    node = HListKey(contents=node.contents,
+                    badness=node.badness + best_route[0].badness)
     # and add it to the front of the route.
     best_route = [node] + best_route
     return best_route
@@ -70,9 +79,9 @@ def get_all_routes(node, tree, w):
 
 
 def h_list_to_best_h_boxes(h_list, h_size):
-    h_box_tree = h_list_to_h_box_tree(h_list, h_size)
-    root_node = [None, None, 0]
-    best_route = get_best_route(root_node, h_box_tree, h_size)
+    box_tree = h_list_to_box_tree(h_list, h_size)
+    root_node = HListKey(contents=None, badness=0)
+    best_route = get_best_route(root_node, box_tree, h_size)
     # Ignore root node.
     contents = best_route[1:]
     h_boxes = [HBox(contents=c[0], to=h_size, set_glue=True)
