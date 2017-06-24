cfb11de05005819b35f1543939bfa6bbcb49a604
Elliot Marsden <elliot.marsden@gmail.com>
2017-06-24 13:48:38 +0200

    Simplify defining parameters and special to ease mypy analysis

--- nex/constants/parameters.py
+++ nex/constants/parameters.py
@@ -10,113 +10,218 @@ from enum import Enum
 from .instructions import Instructions
 from ..tokens import instructions_to_types
 
-params = {
-    'PRE_TOLERANCE': Instructions.integer_parameter,
-    'TOLERANCE': Instructions.integer_parameter,
-    'H_BADNESS': Instructions.integer_parameter,
-    'V_BADNESS': Instructions.integer_parameter,
-    'LINE_PENALTY': Instructions.integer_parameter,
-    'HYPHEN_PENALTY': Instructions.integer_parameter,
-    'EX_HYPHEN_PENALTY': Instructions.integer_parameter,
-    'BIN_OP_PENALTY': Instructions.integer_parameter,
-    'REL_PENALTY': Instructions.integer_parameter,
-    'CLUB_PENALTY': Instructions.integer_parameter,
-    'WIDOW_PENALTY': Instructions.integer_parameter,
-    'DISPLAY_WIDOW_PENALTY': Instructions.integer_parameter,
-    'BROKEN_PENALTY': Instructions.integer_parameter,
-    'PRE_DISPLAY_PENALTY': Instructions.integer_parameter,
-    'POST_DISPLAY_PENALTY': Instructions.integer_parameter,
-    'INTER_LINE_PENALTY': Instructions.integer_parameter,
-    'FLOATING_PENALTY': Instructions.integer_parameter,
-    'OUTPUT_PENALTY': Instructions.integer_parameter,
-    'DOUBLE_HYPHEN_DEMERITS': Instructions.integer_parameter,
-    'FINAL_HYPHEN_DEMERITS': Instructions.integer_parameter,
-    'ADJ_DEMERITS': Instructions.integer_parameter,
-    'LOOSENESS': Instructions.integer_parameter,
-    'PAUSING': Instructions.integer_parameter,
-    'HOLDING_INSERTS': Instructions.integer_parameter,
-    'TRACING_ON_LINE': Instructions.integer_parameter,
-    'TRACING_MACROS': Instructions.integer_parameter,
-    'TRACING_STATS': Instructions.integer_parameter,
-    'TRACING_PARAGRAPHS': Instructions.integer_parameter,
-    'TRACING_PAGES': Instructions.integer_parameter,
-    'TRACING_OUTPUT': Instructions.integer_parameter,
-    'TRACING_LOSTCHARS': Instructions.integer_parameter,
-    'TRACING_COMMANDS': Instructions.integer_parameter,
-    'TRACING_RESTORES': Instructions.integer_parameter,
-    'LANGUAGE': Instructions.integer_parameter,
-    'UC_HYPH': Instructions.integer_parameter,
-    'LEFT_HYPHEN_MIN': Instructions.integer_parameter,
-    'RIGHT_HYPHEN_MIN': Instructions.integer_parameter,
-    'GLOBAL_DEFS': Instructions.integer_parameter,
-    'MAX_DEAD_CYCLES': Instructions.integer_parameter,
-    'HANG_AFTER': Instructions.integer_parameter,
-    'FAM': Instructions.integer_parameter,
-    'MAG': Instructions.integer_parameter,
-    'ESCAPE_CHAR': Instructions.integer_parameter,
-    'DEFAULT_HYPHEN_CHAR': Instructions.integer_parameter,
-    'DEFAULT_SKEW_CHAR': Instructions.integer_parameter,
-    'END_LINE_CHAR': Instructions.integer_parameter,
-    'NEW_LINE_CHAR': Instructions.integer_parameter,
-    'DELIMITER_FACTOR': Instructions.integer_parameter,
+
+class Parameters(Enum):
+    pre_tolerance = 'PRE_TOLERANCE'
+    tolerance = 'TOLERANCE'
+    h_badness = 'H_BADNESS'
+    v_badness = 'V_BADNESS'
+    line_penalty = 'LINE_PENALTY'
+    hyphen_penalty = 'HYPHEN_PENALTY'
+    ex_hyphen_penalty = 'EX_HYPHEN_PENALTY'
+    bin_op_penalty = 'BIN_OP_PENALTY'
+    rel_penalty = 'REL_PENALTY'
+    club_penalty = 'CLUB_PENALTY'
+    widow_penalty = 'WIDOW_PENALTY'
+    display_widow_penalty = 'DISPLAY_WIDOW_PENALTY'
+    broken_penalty = 'BROKEN_PENALTY'
+    pre_display_penalty = 'PRE_DISPLAY_PENALTY'
+    post_display_penalty = 'POST_DISPLAY_PENALTY'
+    inter_line_penalty = 'INTER_LINE_PENALTY'
+    floating_penalty = 'FLOATING_PENALTY'
+    output_penalty = 'OUTPUT_PENALTY'
+    double_hyphen_demerits = 'DOUBLE_HYPHEN_DEMERITS'
+    final_hyphen_demerits = 'FINAL_HYPHEN_DEMERITS'
+    adj_demerits = 'ADJ_DEMERITS'
+    looseness = 'LOOSENESS'
+    pausing = 'PAUSING'
+    holding_inserts = 'HOLDING_INSERTS'
+    tracing_on_line = 'TRACING_ON_LINE'
+    tracing_macros = 'TRACING_MACROS'
+    tracing_stats = 'TRACING_STATS'
+    tracing_paragraphs = 'TRACING_PARAGRAPHS'
+    tracing_pages = 'TRACING_PAGES'
+    tracing_output = 'TRACING_OUTPUT'
+    tracing_lostchars = 'TRACING_LOSTCHARS'
+    tracing_commands = 'TRACING_COMMANDS'
+    tracing_restores = 'TRACING_RESTORES'
+    language = 'LANGUAGE'
+    uc_hyph = 'UC_HYPH'
+    left_hyphen_min = 'LEFT_HYPHEN_MIN'
+    right_hyphen_min = 'RIGHT_HYPHEN_MIN'
+    global_defs = 'GLOBAL_DEFS'
+    max_dead_cycles = 'MAX_DEAD_CYCLES'
+    hang_after = 'HANG_AFTER'
+    fam = 'FAM'
+    mag = 'MAG'
+    escape_char = 'ESCAPE_CHAR'
+    default_hyphen_char = 'DEFAULT_HYPHEN_CHAR'
+    default_skew_char = 'DEFAULT_SKEW_CHAR'
+    end_line_char = 'END_LINE_CHAR'
+    new_line_char = 'NEW_LINE_CHAR'
+    delimiter_factor = 'DELIMITER_FACTOR'
     # THESE TIME ONES WILL BE SET IN GET_INITIAL_PARAMETERS.
-    'TIME': Instructions.integer_parameter,
-    'DAY': Instructions.integer_parameter,
-    'MONTH': Instructions.integer_parameter,
-    'YEAR': Instructions.integer_parameter,
-    'SHOW_BOX_BREADTH': Instructions.integer_parameter,
-    'SHOW_BOX_DEPTH': Instructions.integer_parameter,
-    'ERROR_CONTEXT_LINES': Instructions.integer_parameter,
-    'H_FUZZ': Instructions.dimen_parameter,
-    'V_FUZZ': Instructions.dimen_parameter,
-    'OVER_FULL_RULE': Instructions.dimen_parameter,
-    'H_SIZE': Instructions.dimen_parameter,
-    'V_SIZE': Instructions.dimen_parameter,
-    'MAX_DEPTH': Instructions.dimen_parameter,
-    'SPLIT_MAX_DEPTH': Instructions.dimen_parameter,
-    'BOX_MAX_DEPTH': Instructions.dimen_parameter,
-    'LINE_SKIP_LIMIT': Instructions.dimen_parameter,
-    'DELIMITER_SHORT_FALL': Instructions.dimen_parameter,
-    'NULL_DELIMITER_SPACE': Instructions.dimen_parameter,
-    'SCRIPT_SPACE': Instructions.dimen_parameter,
-    'MATH_SURROUND': Instructions.dimen_parameter,
-    'PRE_DISPLAY_SIZE': Instructions.dimen_parameter,
-    'DISPLAY_WIDTH': Instructions.dimen_parameter,
-    'DISPLAY_INDENT': Instructions.dimen_parameter,
-    'PAR_INDENT': Instructions.dimen_parameter,
-    'HANG_INDENT': Instructions.dimen_parameter,
-    'H_OFFSET': Instructions.dimen_parameter,
-    'V_OFFSET': Instructions.dimen_parameter,
-    'BASE_LINE_SKIP': Instructions.glue_parameter,
-    'LINE_SKIP': Instructions.glue_parameter,
-    'PAR_SKIP': Instructions.glue_parameter,
-    'ABOVE_DISPLAY_SKIP': Instructions.glue_parameter,
-    'ABOVE_DISPLAY_SHORT_SKIP': Instructions.glue_parameter,
-    'BELOW_DISPLAY_SKIP': Instructions.glue_parameter,
-    'BELOW_DISPLAY_SHORT_SKIP': Instructions.glue_parameter,
-    'LEFT_SKIP': Instructions.glue_parameter,
-    'RIGHT_SKIP': Instructions.glue_parameter,
-    'TOP_SKIP': Instructions.glue_parameter,
-    'SPLIT_TOP_SKIP': Instructions.glue_parameter,
-    'TAB_SKIP': Instructions.glue_parameter,
-    'SPACE_SKIP': Instructions.glue_parameter,
-    'X_SPACE_SKIP': Instructions.glue_parameter,
-    'PAR_FILL_SKIP': Instructions.glue_parameter,
-    'THIN_MU_SKIP': Instructions.mu_glue_parameter,
-    'MED_MU_SKIP': Instructions.mu_glue_parameter,
-    'THICK_MU_SKIP': Instructions.mu_glue_parameter,
-    'OUTPUT': Instructions.token_parameter,
-    'EVERY_PAR': Instructions.token_parameter,
-    'EVERY_MATH': Instructions.token_parameter,
-    'EVERY_DISPLAY': Instructions.token_parameter,
-    'EVERY_H_BOX': Instructions.token_parameter,
-    'EVERY_V_BOX': Instructions.token_parameter,
-    'EVERY_JOB': Instructions.token_parameter,
-    'EVERY_CR': Instructions.token_parameter,
-    'ERR_HELP': Instructions.token_parameter,
+    time = 'TIME'
+    day = 'DAY'
+    month = 'MONTH'
+    year = 'YEAR'
+    show_box_breadth = 'SHOW_BOX_BREADTH'
+    show_box_depth = 'SHOW_BOX_DEPTH'
+    error_context_lines = 'ERROR_CONTEXT_LINES'
+    h_fuzz = 'H_FUZZ'
+    v_fuzz = 'V_FUZZ'
+    over_full_rule = 'OVER_FULL_RULE'
+    h_size = 'H_SIZE'
+    v_size = 'V_SIZE'
+    max_depth = 'MAX_DEPTH'
+    split_max_depth = 'SPLIT_MAX_DEPTH'
+    box_max_depth = 'BOX_MAX_DEPTH'
+    line_skip_limit = 'LINE_SKIP_LIMIT'
+    delimiter_short_fall = 'DELIMITER_SHORT_FALL'
+    null_delimiter_space = 'NULL_DELIMITER_SPACE'
+    script_space = 'SCRIPT_SPACE'
+    math_surround = 'MATH_SURROUND'
+    pre_display_size = 'PRE_DISPLAY_SIZE'
+    display_width = 'DISPLAY_WIDTH'
+    display_indent = 'DISPLAY_INDENT'
+    par_indent = 'PAR_INDENT'
+    hang_indent = 'HANG_INDENT'
+    h_offset = 'H_OFFSET'
+    v_offset = 'V_OFFSET'
+    base_line_skip = 'BASE_LINE_SKIP'
+    line_skip = 'LINE_SKIP'
+    par_skip = 'PAR_SKIP'
+    above_display_skip = 'ABOVE_DISPLAY_SKIP'
+    above_display_short_skip = 'ABOVE_DISPLAY_SHORT_SKIP'
+    below_display_skip = 'BELOW_DISPLAY_SKIP'
+    below_display_short_skip = 'BELOW_DISPLAY_SHORT_SKIP'
+    left_skip = 'LEFT_SKIP'
+    right_skip = 'RIGHT_SKIP'
+    top_skip = 'TOP_SKIP'
+    split_top_skip = 'SPLIT_TOP_SKIP'
+    tab_skip = 'TAB_SKIP'
+    space_skip = 'SPACE_SKIP'
+    x_space_skip = 'X_SPACE_SKIP'
+    par_fill_skip = 'PAR_FILL_SKIP'
+    thin_mu_skip = 'THIN_MU_SKIP'
+    med_mu_skip = 'MED_MU_SKIP'
+    thick_mu_skip = 'THICK_MU_SKIP'
+    output = 'OUTPUT'
+    every_par = 'EVERY_PAR'
+    every_math = 'EVERY_MATH'
+    every_display = 'EVERY_DISPLAY'
+    every_h_box = 'EVERY_H_BOX'
+    every_v_box = 'EVERY_V_BOX'
+    every_job = 'EVERY_JOB'
+    every_cr = 'EVERY_CR'
+    err_help = 'ERR_HELP'
+
+
+param_to_instr = {
+    Parameters.pre_tolerance: Instructions.integer_parameter,
+    Parameters.tolerance: Instructions.integer_parameter,
+    Parameters.h_badness: Instructions.integer_parameter,
+    Parameters.v_badness: Instructions.integer_parameter,
+    Parameters.line_penalty: Instructions.integer_parameter,
+    Parameters.hyphen_penalty: Instructions.integer_parameter,
+    Parameters.ex_hyphen_penalty: Instructions.integer_parameter,
+    Parameters.bin_op_penalty: Instructions.integer_parameter,
+    Parameters.rel_penalty: Instructions.integer_parameter,
+    Parameters.club_penalty: Instructions.integer_parameter,
+    Parameters.widow_penalty: Instructions.integer_parameter,
+    Parameters.display_widow_penalty: Instructions.integer_parameter,
+    Parameters.broken_penalty: Instructions.integer_parameter,
+    Parameters.pre_display_penalty: Instructions.integer_parameter,
+    Parameters.post_display_penalty: Instructions.integer_parameter,
+    Parameters.inter_line_penalty: Instructions.integer_parameter,
+    Parameters.floating_penalty: Instructions.integer_parameter,
+    Parameters.output_penalty: Instructions.integer_parameter,
+    Parameters.double_hyphen_demerits: Instructions.integer_parameter,
+    Parameters.final_hyphen_demerits: Instructions.integer_parameter,
+    Parameters.adj_demerits: Instructions.integer_parameter,
+    Parameters.looseness: Instructions.integer_parameter,
+    Parameters.pausing: Instructions.integer_parameter,
+    Parameters.holding_inserts: Instructions.integer_parameter,
+    Parameters.tracing_on_line: Instructions.integer_parameter,
+    Parameters.tracing_macros: Instructions.integer_parameter,
+    Parameters.tracing_stats: Instructions.integer_parameter,
+    Parameters.tracing_paragraphs: Instructions.integer_parameter,
+    Parameters.tracing_pages: Instructions.integer_parameter,
+    Parameters.tracing_output: Instructions.integer_parameter,
+    Parameters.tracing_lostchars: Instructions.integer_parameter,
+    Parameters.tracing_commands: Instructions.integer_parameter,
+    Parameters.tracing_restores: Instructions.integer_parameter,
+    Parameters.language: Instructions.integer_parameter,
+    Parameters.uc_hyph: Instructions.integer_parameter,
+    Parameters.left_hyphen_min: Instructions.integer_parameter,
+    Parameters.right_hyphen_min: Instructions.integer_parameter,
+    Parameters.global_defs: Instructions.integer_parameter,
+    Parameters.max_dead_cycles: Instructions.integer_parameter,
+    Parameters.hang_after: Instructions.integer_parameter,
+    Parameters.fam: Instructions.integer_parameter,
+    Parameters.mag: Instructions.integer_parameter,
+    Parameters.escape_char: Instructions.integer_parameter,
+    Parameters.default_hyphen_char: Instructions.integer_parameter,
+    Parameters.default_skew_char: Instructions.integer_parameter,
+    Parameters.end_line_char: Instructions.integer_parameter,
+    Parameters.new_line_char: Instructions.integer_parameter,
+    Parameters.delimiter_factor: Instructions.integer_parameter,
+    # THESE TIME ONES WILL BE SET IN GET_INITIAL_PARAMETERS.
+    Parameters.time: Instructions.integer_parameter,
+    Parameters.day: Instructions.integer_parameter,
+    Parameters.month: Instructions.integer_parameter,
+    Parameters.year: Instructions.integer_parameter,
+    Parameters.show_box_breadth: Instructions.integer_parameter,
+    Parameters.show_box_depth: Instructions.integer_parameter,
+    Parameters.error_context_lines: Instructions.integer_parameter,
+    Parameters.h_fuzz: Instructions.dimen_parameter,
+    Parameters.v_fuzz: Instructions.dimen_parameter,
+    Parameters.over_full_rule: Instructions.dimen_parameter,
+    Parameters.h_size: Instructions.dimen_parameter,
+    Parameters.v_size: Instructions.dimen_parameter,
+    Parameters.max_depth: Instructions.dimen_parameter,
+    Parameters.split_max_depth: Instructions.dimen_parameter,
+    Parameters.box_max_depth: Instructions.dimen_parameter,
+    Parameters.line_skip_limit: Instructions.dimen_parameter,
+    Parameters.delimiter_short_fall: Instructions.dimen_parameter,
+    Parameters.null_delimiter_space: Instructions.dimen_parameter,
+    Parameters.script_space: Instructions.dimen_parameter,
+    Parameters.math_surround: Instructions.dimen_parameter,
+    Parameters.pre_display_size: Instructions.dimen_parameter,
+    Parameters.display_width: Instructions.dimen_parameter,
+    Parameters.display_indent: Instructions.dimen_parameter,
+    Parameters.par_indent: Instructions.dimen_parameter,
+    Parameters.hang_indent: Instructions.dimen_parameter,
+    Parameters.h_offset: Instructions.dimen_parameter,
+    Parameters.v_offset: Instructions.dimen_parameter,
+    Parameters.base_line_skip: Instructions.glue_parameter,
+    Parameters.line_skip: Instructions.glue_parameter,
+    Parameters.par_skip: Instructions.glue_parameter,
+    Parameters.above_display_skip: Instructions.glue_parameter,
+    Parameters.above_display_short_skip: Instructions.glue_parameter,
+    Parameters.below_display_skip: Instructions.glue_parameter,
+    Parameters.below_display_short_skip: Instructions.glue_parameter,
+    Parameters.left_skip: Instructions.glue_parameter,
+    Parameters.right_skip: Instructions.glue_parameter,
+    Parameters.top_skip: Instructions.glue_parameter,
+    Parameters.split_top_skip: Instructions.glue_parameter,
+    Parameters.tab_skip: Instructions.glue_parameter,
+    Parameters.space_skip: Instructions.glue_parameter,
+    Parameters.x_space_skip: Instructions.glue_parameter,
+    Parameters.par_fill_skip: Instructions.glue_parameter,
+    Parameters.thin_mu_skip: Instructions.mu_glue_parameter,
+    Parameters.med_mu_skip: Instructions.mu_glue_parameter,
+    Parameters.thick_mu_skip: Instructions.mu_glue_parameter,
+    Parameters.output: Instructions.token_parameter,
+    Parameters.every_par: Instructions.token_parameter,
+    Parameters.every_math: Instructions.token_parameter,
+    Parameters.every_display: Instructions.token_parameter,
+    Parameters.every_h_box: Instructions.token_parameter,
+    Parameters.every_v_box: Instructions.token_parameter,
+    Parameters.every_job: Instructions.token_parameter,
+    Parameters.every_cr: Instructions.token_parameter,
+    Parameters.err_help: Instructions.token_parameter,
 }
-Parameters = Enum('Parameters', {s.lower(): s for s in params})
-param_to_instr = {p: params[p.value] for p in Parameters}
 param_to_type = {p: instr.value for p, instr in param_to_instr.items()}
 
 param_instrs = (
--- nex/constants/specials.py
+++ nex/constants/specials.py
@@ -10,13 +10,14 @@ from enum import Enum
 from .instructions import Instructions
 from ..tokens import instructions_to_types
 
-specials = {
-    'SPACE_FACTOR': Instructions.special_integer,
+
+class Specials(Enum):
+    space_factor = 'SPACE_FACTOR'
     # The number of lines in the paragraph most recently completed or partially
     # completed.
-    'PREV_GRAF': Instructions.special_integer,
+    prev_graf = 'PREV_GRAF'
     # The number of times \output was called since the last \shipout.
-    'DEAD_CYCLES': Instructions.special_integer,
+    dead_cycles = 'DEAD_CYCLES'
     # Means different things:
     # 1. In the ouput routine, the total number of held-over insertions. For
     #    each class of insertions this includes the unused part of a split
@@ -24,29 +25,63 @@ specials = {
     #    page.
     # 2. In the page-making routine, the total of the \floatingpenalty for each
     #    unsplit insertion which is carried over to the next page.
-    'INSERT_PENALTIES': Instructions.special_integer,
+    insert_penalties = 'INSERT_PENALTIES'
 
     # The depth of the last box added to the current vertical list.
-    'PREV_DEPTH': Instructions.special_dimen,
+    prev_depth = 'PREV_DEPTH'
     # The actual depth of the last box on the main page.
-    'PAGE_DEPTH': Instructions.special_dimen,
+    page_depth = 'PAGE_DEPTH'
     # The desired height of the current page.
-    'PAGE_GOAL': Instructions.special_dimen,
+    page_goal = 'PAGE_GOAL'
     # The accumulated height of the current page.
-    'PAGE_TOTAL': Instructions.special_dimen,
+    page_total = 'PAGE_TOTAL'
     # The amount of finite stretchability in the current page.
-    'PAGE_STRETCH': Instructions.special_dimen,
+    page_stretch = 'PAGE_STRETCH'
     # The amount of first-order infinite stretchability in the current page.
-    'PAGE_FIL_STRETCH': Instructions.special_dimen,
+    page_fil_stretch = 'PAGE_FIL_STRETCH'
     # The amount of second-order infinite stretchability in the current page.
-    'PAGE_FILL_STRETCH': Instructions.special_dimen,
+    page_fill_stretch = 'PAGE_FILL_STRETCH'
     # The amount of third-order infinite stretchability in the current page.
-    'PAGE_FILLL_STRETCH': Instructions.special_dimen,
+    page_filll_stretch = 'PAGE_FILLL_STRETCH'
     # The amount of finite shrinkability in the current page.
-    'PAGE_SHRINK': Instructions.special_dimen,
+    page_shrink = 'PAGE_SHRINK'
+
+
+special_to_instr = {
+    Specials.space_factor: Instructions.special_integer,
+    # The number of lines in the paragraph most recently completed or partially
+    # completed.
+    Specials.prev_graf: Instructions.special_integer,
+    # The number of times \output was called since the last \shipout.
+    Specials.dead_cycles: Instructions.special_integer,
+    # Means different things:
+    # 1. In the ouput routine, the total number of held-over insertions. For
+    #    each class of insertions this includes the unused part of a split
+    #    insertion and all other insertions which don't appear on the current
+    #    page.
+    # 2. In the page-making routine, the total of the \floatingpenalty for each
+    #    unsplit insertion which is carried over to the next page.
+    Specials.insert_penalties: Instructions.special_integer,
+
+    # The depth of the last box added to the current vertical list.
+    Specials.prev_depth: Instructions.special_dimen,
+    # The actual depth of the last box on the main page.
+    Specials.page_depth: Instructions.special_dimen,
+    # The desired height of the current page.
+    Specials.page_goal: Instructions.special_dimen,
+    # The accumulated height of the current page.
+    Specials.page_total: Instructions.special_dimen,
+    # The amount of finite stretchability in the current page.
+    Specials.page_stretch: Instructions.special_dimen,
+    # The amount of first-order infinite stretchability in the current page.
+    Specials.page_fil_stretch: Instructions.special_dimen,
+    # The amount of second-order infinite stretchability in the current page.
+    Specials.page_fill_stretch: Instructions.special_dimen,
+    # The amount of third-order infinite stretchability in the current page.
+    Specials.page_filll_stretch: Instructions.special_dimen,
+    # The amount of finite shrinkability in the current page.
+    Specials.page_shrink: Instructions.special_dimen,
 }
-Specials = Enum('Specials', {s.lower(): s for s in specials})
-special_to_instr = {p: specials[p.value] for p in Specials}
 special_to_type = {p: instr.value for p, instr in special_to_instr.items()}
 
 special_instrs = (
