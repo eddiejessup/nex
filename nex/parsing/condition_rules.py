30f323f80cf3c2b07824e7bfafdb5eeba6cbd51d
Elliot Marsden <elliot.marsden@gmail.com>
2017-06-25 12:25:29 +0200

    Add stubs for parsing if conditions

--- nex/parsing/condition_rules.py
+++ nex/parsing/condition_rules.py
@@ -2,14 +2,6 @@ from ..tokens import BuiltToken
 
 
 def add_condition_rules(pg):
-    @pg.production('condition : IF_TRUE')
-    def condition_if_true(p):
-        return BuiltToken(type_=p[0].type, value=None)
-
-    @pg.production('condition : IF_FALSE')
-    def condition_if_false(p):
-        return BuiltToken(type_=p[0].type, value=None)
-
     @pg.production('condition : IF_NUM number relation number')
     def condition_if_num(p):
         return BuiltToken(type_=p[0].type,
@@ -29,19 +21,45 @@ def add_condition_rules(pg):
         return BuiltToken(type_=p[0].type, value={'number': p[1]})
 
     @pg.production('condition : IF_V_MODE')
-    def condition_if_v_mode(p):
+    @pg.production('condition : IF_H_MODE')
+    @pg.production('condition : IF_M_MODE')
+    @pg.production('condition : IF_INNER')
+    def condition_if_mode(p):
         return BuiltToken(type_=p[0].type, value=None)
 
-    @pg.production('condition : IF_H_MODE')
-    def condition_if_h_mode(p):
+    # @pg.production('condition : IF_CHAR somethingsomething')
+    # def condition_if_char(p):
+    #     return BuiltToken(type_=p[0].type, value=None)
+
+    # @pg.production('condition : IF_CAT somethingsomething')
+    # def condition_if_cat(p):
+    #     return BuiltToken(type_=p[0].type, value=None)
+
+    # @pg.production('condition : IF_TOKEN somethingsomething')
+    # def condition_if_token(p):
+    #     return BuiltToken(type_=p[0].type, value=None)
+
+    # @pg.production('condition : IF_VOID somethingsomething')
+    # @pg.production('condition : IF_H_BOX somethingsomething')
+    # @pg.production('condition : IF_V_BOX somethingsomething')
+    # def condition_if_box(p):
+    #     return BuiltToken(type_=p[0].type, value=None)
+
+    # @pg.production('condition : IF_END_OF_FILE somethingsomething')
+    # def condition_if_end_of_file(p):
+    #     return BuiltToken(type_=p[0].type, value=None)
+
+    @pg.production('condition : IF_TRUE')
+    @pg.production('condition : IF_FALSE')
+    def condition_if_boolean(p):
         return BuiltToken(type_=p[0].type, value=None)
+
+    @pg.production('condition : IF_CASE number')
+    def condition_if_case(p):
+        return BuiltToken(type_=p[0].type, value={'number': p[1]})
 
     @pg.production('relation : LESS_THAN')
     @pg.production('relation : EQUALS')
     @pg.production('relation : GREATER_THAN')
     def relation(p):
         return p[0]
-
-    @pg.production('condition : IF_CASE number')
-    def condition_if_case(p):
-        return BuiltToken(type_=p[0].type, value={'number': p[1]})
